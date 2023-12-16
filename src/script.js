d71e994afb9df74a09a385ad482ac01199ca3303
Vlad Prudius <vladvlpr006@gmail.com>
2023-12-16 09:28:01 +0200

    Added loadTodayWeather logic

--- src/script.js
+++ src/script.js
@@ -35,18 +35,16 @@ function getWeatherIconPath(iconName){
   return `../assets/icons/${iconName}.png`;
 }
 
-async function loadWeatherDataToTable(latitude, longitude, size=6) {
+async function loadHourlyWeatherDataToTable(latitude, longitude, size=6) {
   const timeRow = document.getElementById('today-weather-time-row');
   const forecastRow = document.getElementById('today-weather-forecast-row');
   const tempRow = document.getElementById('today-weather-temperature-row');
   const realFeelRow = document.getElementById('today-weather-realfeel-row');
   const windRow = document.getElementById('today-weather-wind-row');
-  const currentLocation = document.getElementById('current-weather-location');
+  
 
   try{
     const data = await fetchWeatherData(latitude, longitude);
-    const location = `${data.city.name}, ${data.city.country}`;
-    currentLocation.innerHTML = `<p>${location}</p>`;
     data.list.slice(0, size).forEach(item => {
       const weather = new HourlyWeather(
         item.dt_txt,
@@ -63,6 +61,32 @@ async function loadWeatherDataToTable(latitude, longitude, size=6) {
       realFeelRow.innerHTML += weather.render_realFeel();
       windRow.innerHTML += weather.render_wind();
     });
+  }
+  catch(error){
+    console.log(error);
+  }
+}
+
+async function loadTodayWeatherData(latitude, longitude) {
+  const currentDateElement = document.getElementById('current-weather-date');
+  const currentLocation = document.getElementById('current-weather-location');
+  const todayWeatherBox = document.getElementById('today-weather-box');
+  currentDateElement.innerHTML = `<p>${new Date().toDateString()}</p>`;
+
+  try{
+    const data = await fetchWeatherData(latitude, longitude, false);
+    const weather = new TodayWeather(
+      data.dt,
+      data.weather[0].icon,
+      data.weather[0].description,
+      data.main.temp,
+      data.main.feels_like,
+      data.sys.sunrise,
+      data.sys.sunset
+    );
+    currentLocation.innerHTML = `<p>${data.name}, ${data.sys.country}</p>`;
+    todayWeatherBox.innerHTML += weather.render();
+  
   }
   catch(error){
     console.log(error);
@@ -112,7 +136,46 @@ class HourlyWeather{
     }
 }
 
+class TodayWeather{
+  constructor(date, iconName, forecast, temp, realFeel, sunrise, sunset){
+    this.date = date;
+    this.iconName = iconName;
+    this.forecast = forecast;
+    this.temp = temp;
+    this.realFeel = realFeel;
+    this.sunrise = sunrise;
+    this.sunset = sunset;
+  }
+
+  render(){
+    const options = { hour: '2-digit', minute: '2-digit', timeZone: 'UTC' };
+    const sunriseTime = new Date(this.sunrise).toLocaleTimeString('en-US', options);
+    const sunsetTime = new Date(this.sunset).toLocaleTimeString('en-US', options);
+
+    return `<div class="today-weather-icon">
+    <img src="${getWeatherIconPath(this.iconName)}">
+    </div>
+    <div class="today-weather-forecast">
+    <p>${this.forecast}</p>
+    </div>
+    <div class="today-weather-temperature">
+    <p>${this.temp}</p>
+    </div>
+    <div class="today-weather-realFeel">
+    <p>${this.realFeel}</p>
+    </div>
+    <div class="today-weather-sunrise">
+    <p>${sunriseTime}</p>
+    </div>
+    <div class="today-weather-sunset">
+    <p>${sunsetTime}</p>
+    </div>`
+  }
+
+}
+
 async function getinfo(){
   const coordinates = await getCoordinates();
-  await loadWeatherDataToTable(coordinates[0], coordinates[1]);
+  await loadTodayWeatherData(coordinates[0], coordinates[1]);
+  await loadHourlyWeatherDataToTable(coordinates[0], coordinates[1]);
 }
